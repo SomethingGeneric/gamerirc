e71994acef55cb8fa251b50aa6be43b7d9183b8e
Matt C <matthew.compton@mymail.champlain.edu>
2023-03-09 10:26:24 -0500

    weee kinda works

--- main.py
+++ main.py
@@ -1,14 +1,82 @@
-import pydle
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 
-# Simple echo bot.
-class MyOwnBot(pydle.Client):
-    async def on_connect(self):
-         await self.join('#bottest')
+import subprocess
 
-    async def on_message(self, target, source, message):
-         # don't respond to our own messages, as this leads to a positive feedback loop
-         if source != self.nickname:
-            await self.message(target, message)
 
-client = MyOwnBot('gamerbot', realname='GamerTheBot')
-client.run('xhec.dev', tls=False, tls_verify=False)
+class TestBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(
+            self.connection.get_nickname()
+        ):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        # non-chat DCC messages are raw bytes; decode as text
+        text = e.arguments[0].decode("utf-8")
+        c.privmsg("You said: " + text)
+
+    def on_dccchat(self, c, e):
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = sorted(chobj.users())
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = sorted(chobj.opers())
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = sorted(chobj.voiced())
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        elif cmd == "kernel":
+            me = subprocess.check_output(["uname", "-r"])
+            c.notice(nick, f"I have: {me.decode().strip()}.")
+        # elif cmd == "dcc":
+        #    dcc = self.dcc_listen()
+        #    c.ctcp(
+        #        "DCC",
+        #        nick,
+        #        "CHAT chat %s %d"
+        #        % (ip_quad_to_numstr(dcc.localaddress), dcc.localport),
+        #    )
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+
+
+if __name__ == "__main__":
+    bot = TestBot("#bottest", "gamerbot", "xhec.dev", 6667)
+    bot.start()
